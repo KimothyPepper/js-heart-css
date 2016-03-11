3245543f0ef00e0f8dea042d2037ebe0b7cf5bfe
KimothyPepper <kim-pep@hotmail.com>
2016-03-10 19:29:20 -0500

    Added Content to js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $p = $('p');
+var $h2 = $('h2');
+var $img = $('img');
+var $dl = $('dl');
+var $dt =$('dt');
+
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+.addClass('intro');
+$h2.addclass('slug-head');
+.addClass('slug-list');
+$img.addClass('slug-img');
+.addClass('slug-list-first');
+.addClass('slug-list-last');
+$dl.addClass('classification');
+$dt.removeClass('big-label');
